a03197b69d25f163c1340d766d5c6dedfdeb6385
Belhenniche Yassaa <Yagon66@gmail.com>
2019-05-24 19:23:54 +0100

    feat: add calculations logic

--- src/components/CalcPage.js
+++ src/components/CalcPage.js
@@ -1,40 +1,79 @@
 import React, { Component } from 'react'
 import Branches from '../data/Branches.json'
-import { taggedTemplateExpression } from '@babel/types';
 
 class CalcPage extends Component {
+  state = {
+    branch: this.props.branch,
+    subjects: [],
+    allCoefficients: 0,
+    allMarks: [],
+    average: 0
+  }
+
+  componentDidMount() {
+    const BRANCH = this.props.branch;
+    const SUBJECTS = Branches[BRANCH];
+    let allCoefficients = 0;
+    let allMarks = [];
+    SUBJECTS.map((subject, index) => {
+      allCoefficients += subject.coefficient;
+      allMarks.push(0);
+    });
+    this.setState({
+      subjects: SUBJECTS,
+      allCoefficients,
+      allMarks
+    });
+  }
+
+  calculateMarks = (mark, coefficient, subjectIndex) => {
+    const MARK_RESULT = document.querySelector(`.markResult-${subjectIndex}`);
+    const TOTAL_MARKS_RESULTS = document.querySelector(`.row-result .col:last-of-type`);
+    const AVERAGE = document.querySelector(`.average`);
+    let allMarksResult = 0;
+    let markResult = Number((mark * coefficient).toFixed(2));
+    MARK_RESULT.innerHTML = markResult;
+    this.setState(state => {
+      state.allMarks[subjectIndex] = markResult;
+      state.allMarks.map(mark => allMarksResult += mark);
+      TOTAL_MARKS_RESULTS.innerHTML = allMarksResult;
+      AVERAGE.innerHTML = (allMarksResult / this.state.allCoefficients).toFixed(2);
+    });
+  };
 
   render() {
-    const BRANCH = this.props.branch;
-    const SUBJECTS = Object.values(Branches[BRANCH]);
-    let allCoefficients = 0;
-    SUBJECTS.map(subject => allCoefficients += subject.coefficient)
-    console.log(SUBJECTS);
-
     return (
       <div>
-        <h3>CalcPage: {BRANCH}</h3>
-
+        <h3>CalcPage: {this.state.branch}</h3>
         <div className="table">
           <div className="row row-head">
             <div className="col">المادة</div>
-            <div className="col">النقطة/20</div>
+            <div className="col">النقطة</div>
             <div className="col">المعامل</div>
-            <div className="col">{allCoefficients}</div>
+            <div className="col">الناتج</div>
           </div>
-          {SUBJECTS.map((subject, index) => (
+          {this.state.subjects.map((subject, index) => (
             <div className="row" key={index}>
               <div className="col">{subject.name}</div>
-              <div className="col">0
-                {/* <input name={subject.name} type="number" min="0" max="20" placeholder="0" onChange={this.handleChange} /> */}
+              <div className="col">
+                <input name={subject.name} type="number" min="0" max="20" placeholder="0/20" onChange={e => {
+                  this.calculateMarks(e.target.value, subject.coefficient, index);
+                }} />
               </div>
               <div className="col">{subject.coefficient}</div>
-              <div className="col">0</div>
+              <div className={`col markResult-${index}`}>0</div>
             </div>
           ))}
+          <div className="row row-result">
+            <div className="col">المجموع</div>
+            <div className="col">{this.state.allCoefficients}</div>
+            <div className="col">0</div>
+          </div>
+          <div className="row row-result">
+            <div className="col">المعدل</div>
+            <div className="col average">0</div>
+          </div>
         </div>
-
-        {/* <p>{SUBJECTS.subject_01.name}</p> */}
       </div>
     )
   }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,12 +6,24 @@ class MainPage extends Component {
     return (
       <>
         <ul className="branches">
-          <li className="branch"><Link to="/math">Math</Link></li>
-          <li className="branch"><Link to="/science">Science</Link></li>
-          <li className="branch"><Link to="/literature">Literature</Link></li>
-          <li className="branch"><Link to="/languages">Languages</Link></li>
-          <li className="branch"><Link to="/management">Management</Link></li>
-          <li className="branch"><Link to="/tech">Tech</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/math">Math</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/science">Science</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/literature">Literature</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/languages">Languages</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/management">Management</Link></li>
+          <li className="branch">
+            <img src="#"  alt="empty" />
+            <Link to="/tech">Tech</Link></li>
         </ul>
       </>
     )
