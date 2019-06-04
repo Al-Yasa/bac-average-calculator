232e4d4b8ed235d768e88116fc44952ca48f63d4
Belhenniche Yassaa <Yagon66@gmail.com>
2019-06-04 19:13:05 +0100

    Updates

---
+++ precache-manifest.ffb39a55ec030face372e02b97c2969b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7caace9f75028a981dcf49665ace04",
+    "url": "/calculate-bac/index.html"
+  },
+  {
+    "revision": "5298838294d701d9acd2",
+    "url": "/calculate-bac/static/css/main.0ffd45cf.chunk.css"
+  },
+  {
+    "revision": "a3f4f3a9a88d903d2f7a",
+    "url": "/calculate-bac/static/js/2.aead6e3e.chunk.js"
+  },
+  {
+    "revision": "5298838294d701d9acd2",
+    "url": "/calculate-bac/static/js/main.7ff0549b.chunk.js"
+  },
+  {
+    "revision": "5005ccc4ceb39daaee71",
+    "url": "/calculate-bac/static/js/runtime~main.b6091d9c.js"
+  }
+]);
